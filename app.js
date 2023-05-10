d30690c5fda0b0be046a04913af4ccfe21e222d2
Parvathi <p4parvathy214@gmail.com>
2023-05-10 19:36:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const csvParser = require('csv-parser');
+const bodyparser = require('body-parser');
+const app =express();
+
+
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+const mongoose = require('./db')
+const api = require('./routes/api')
+app.use('/api',api)
+
+const learner = require('./routes/learner')
+app.use('/api',learner)
+
+app.listen(2341,()=>{
+    console.log("server running at 2341")
+})
